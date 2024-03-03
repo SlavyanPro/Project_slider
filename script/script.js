b39fafb1c73c75278d0fd967f0afe0379cdf8f9c
SlavyanPro <95080055+SlavyanPro@users.noreply.github.com>
2024-03-03 21:48:15 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,147 @@
+let images = [{
+    url: "images/slider_img_1.jpg",
+    title: "ROSTOV-ON-DON, ARMIRAL"
+  }, {
+    url: "images/slider_img_2.jpg",
+    title: "SOCHI THIEVES"
+  }, {
+    url: "images/slider_img_3.jpg",
+    title: "ROSTOV-ON-DON PATRIOTIC"
+  }
+];
+
+function initSlider(options) {
+  if (!images || !images.length) return;
+  
+  options = options || {
+    titles: true,
+    dots: true,
+    autoplay: false,
+  };
+  
+  let sliderImages = document.querySelector(".slider__images");
+  let sliderArrows = document.querySelector(".slider__arrows");
+  let sliderDots = document.querySelector(".slider__dots");
+  let sliderNav = document.querySelector(".slider__nav");
+  
+  initImages();
+  initArrows();
+  initNav();
+
+  if (options.dots) {
+    initDots();
+  }
+  
+  if (options.titles) {
+    initTitles();
+  }
+  
+  if (options.autoplay) {
+    initAutoplay();
+  }
+  
+  function initImages() {
+    images.forEach((image, index) => {
+      let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
+      sliderImages.innerHTML += imageDiv;
+    });
+  }
+  
+  function initArrows() {
+    sliderArrows.querySelectorAll(".slider__arrow").forEach(arrow => {
+      arrow.addEventListener("click", function() {
+        let curNumber = +sliderImages.querySelector(".active").dataset.index;
+        let nextNumber;
+        if (arrow.classList.contains("left")) {
+          nextNumber = curNumber === 0? images.length - 1 : curNumber - 1;
+        } else {
+          nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
+        }
+        moveSlider(nextNumber);
+      });
+    });
+  }
+  
+  function initDots() {
+    images.forEach((image, index) => {
+      let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
+      sliderDots.innerHTML += dot;
+    });
+    sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
+      dot.addEventListener("click", function() {
+        moveSlider(this.dataset.index);
+        sliderDots.querySelector(".active").classList.remove("active");
+        this.classList.add("active");
+      })
+    })
+  }
+
+  function initNav() {
+    images.forEach((image, index) => {
+      let nav = `<div class="slider__nav-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
+      sliderNav.innerHTML += nav;
+    });
+    sliderNav.querySelectorAll(".slider__nav-item").forEach(nav => {
+      nav.addEventListener("click", function() {
+        moveSlider(this.dataset.index);
+        sliderNav.querySelector(".active").classList.remove("active");
+        this.classList.add("active");
+      })
+    })
+  }
+
+  function moveSlider(num) {
+    sliderImages.querySelector(".active").classList.remove("active");
+    sliderImages.querySelector(".n" + num).classList.add("active");
+    sliderNav.querySelector(".active").classList.remove("active");
+    sliderNav.querySelector(".n" + num).classList.add("active");
+    if (options.dots) {
+      sliderDots.querySelector(".active").classList.remove("active");
+      sliderDots.querySelector(".n" + num).classList.add("active");
+    }
+    if (options.titles) changeTitle(num);
+  }
+  
+  function initTitles() {
+    let titleDiv = `<div class="slider__images-title">${images[0].title}</div>`;
+    sliderImages.innerHTML += cropTitle(titleDiv, 150);
+  }
+  
+  function changeTitle(num) {
+    if (!images[num].title) return;
+    let sliderTitle = sliderImages.querySelector(".slider__images-title");
+    sliderTitle.innerText = cropTitle(images[num].title, 150);
+  }
+
+  // function changeNav(num) {
+  //   if (!images{num}.nav) return;
+  //   let sliderNav = sliderImages.querySelector(".slider__images")
+  // }
+  
+  function cropTitle(title, size) {
+    if (title.length <= size) {
+      return title;
+    } else {
+      return title.substr(0, size) + "...";
+    }
+  }
+  
+  function initAutoplay() {
+    setInterval(() => {
+      let curNumber = +sliderImages.querySelector(".active").dataset.index;
+      let nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
+      moveSlider(nextNumber);
+    }, options.autoplayInterval);
+  }
+}
+
+let sliderOptions = {
+  dots: true,
+  titles: false,
+  autoplay: false,
+  autoplayInterval: 5000
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  initSlider(sliderOptions);
+});
